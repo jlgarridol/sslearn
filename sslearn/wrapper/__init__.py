d59c60980f04edd6b66007f378960b538dd571b5
José Luis Garrido-Labrador <jlgarrido@ubu.es>
2022-05-09 19:55:09 +0200

    CoForest added to importable class

--- sslearn/wrapper/__init__.py
+++ sslearn/wrapper/__init__.py
@@ -1,5 +1,5 @@
 from ._self import SelfTraining, Setred
-from ._co import CoTraining, CoTrainingByCommittee, DemocraticCoLearning, Rasco, RelRasco, TriTraining
+from ._co import CoTraining, CoTrainingByCommittee, DemocraticCoLearning, Rasco, RelRasco, TriTraining, CoForest
 from ._edit_co import DeTriTraining
 
 __all__ = ['SelfTraining', 'CoTrainingByCommittee', 'Rasco', 'RelRasco', 'TriTraining',
