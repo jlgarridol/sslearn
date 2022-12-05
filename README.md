ebac3790f36ef544f8068da0b3663acd1eb25230
Jose Luis Garrido-Labrador <jlgarrido@ubu.es>
2022-12-05 13:36:07 +0000

    Create setup.py

---
+++ README.md
@@ -0,0 +1,38 @@
+Semi-Supervised Learning Library (sslearn)
+===
+
+![Code Climate maintainability](https://img.shields.io/codeclimate/maintainability-percentage/jlgarridol/sslearn) ![Code Climate coverage](https://img.shields.io/codeclimate/coverage/jlgarridol/sslearn) 
+
+The `sslearn` is a Python package for machine learning over Semi-supervised datasets. It is an extension of [scikit-learn](https://github.com/scikit-learn/scikit-learn).
+
+Installation
+---
+### Dependencies
+
+* scikit_learn = 1.1.2
+* joblib = 1.1.0
+* numpy = 1.23.3
+* pandas = 1.4.3
+* scipy = 1.9.3
+* statsmodels = 0.13.2
+* pytest = 7.2.0 (only for testing)
+
+### `pip` installation
+
+It can be installed using *Pypi*:
+
+    pip install sslearn
+
+How to
+---
+```python
+from sslearn.wrapper import TriTraining
+from sslearn.model_selection import artificial_ssl_dataset
+from sklearn.datasets import load_iris
+
+X, y = load_iris(return_X_y=True)
+X, y, X_unlabel, true_label = artificial_ssl_dataset(X, y, label_rate=0.1)
+
+model = TriTraining().fit(X, y)
+model.score(X_unlabel, true_label)
+```
---
+++ setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+with open('README.md') as f:
+    readme = f.read()
+
+setuptools.setup(
+        name='sslearn',
+        version='1.0.0',
+        description='A Python package for semi-supervised learning with scikit-learn',
+        long_description=readme,
+        long_description_content_type='text/markdown',
+        author='José Luis Garrido-Labrador',
+        author_email='jlgarrido@ubu.es',
+        url='https://github.com/jlgarridol/sslearn',
+        download_url='https://github.com/jlgarridol/sslearn/archive/v1_0_0.tar.gz',
+        license='BSD 3-Clause License',
+        install_requires=[
+            """joblib==1.1.0
+            numpy==1.23.3
+            pandas==1.4.3
+            pytest==7.2.0
+            scikit_learn==1.1.2
+            scipy==1.9.3
+            statsmodels==0.13.2"""
+        ],
+        packages=setuptools.find_packages(exclude=("tests","experiments")),
+        include_package_data=True,
+        classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: Science/Research',
+            'Topic :: Scientific/Engineering',
+            'License :: OSI Approved :: BSD 3-Clause License',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
+            'Programming Language :: Python :: 3.10'
+        ]
+)
