a6d31a5bf76c4847b71941633d4502f15144ae8f
José Luis Garrido-Labrador <jlgarrido@ubu.es>
2023-02-17 14:45:36 +0100

    Update github actions

--- sslearn/__init__.py
+++ sslearn/__init__.py
@@ -1,4 +1,4 @@
-__VERSION__ = "1.0.3"  # Version of the package
-__AUTHOR__ = "José Luis Garrido-Labrador"  # Author of the package
-__AUTHOR_EMAIL__ = "jlgarrido@ubu.es"  # Author's email
-__URL__ = "https://pypi.org/project/sslearn/"
+__version__='1.0.3'
+__AUTHOR__="José Luis Garrido-Labrador"  # Author of the package
+__AUTHOR_EMAIL__="jlgarrido@ubu.es"  # Author's email
+__URL__="https://pypi.org/project/sslearn/"
