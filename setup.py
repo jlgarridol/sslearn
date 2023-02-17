e9204c1a8481440ca2649289575b4a3b1cafb0b4
José Luis Garrido-Labrador <jlgarrido@ubu.es>
2023-02-17 13:12:43 +0100

    Coverage tests repaired

--- setup.py
+++ setup.py
@@ -21,28 +21,28 @@ version = get_version()
 url = f"https://github.com/jlgarridol/sslearn/archive/refs/tags/f{version}.tar.gz"
 
 setuptools.setup(
-        name='sslearn',
-        version=version,
-        description='A Python package for semi-supervised learning with scikit-learn',
-        long_description=readme,
-        long_description_content_type='text/markdown',
-        author='José Luis Garrido-Labrador',
-        author_email='jlgarrido@ubu.es',
-        url='https://github.com/jlgarridol/sslearn',
-        license='new BSD',
-        download_url=url,
-        install_requires=parse_requirements('requirements.txt'),
-        packages=setuptools.find_packages(exclude=("tests","experiments")),
-        include_package_data=True,
-        classifiers=[
-            'Development Status :: 5 - Production/Stable',
-            'Intended Audience :: Science/Research',
-            'Topic :: Scientific/Engineering',
-            'License :: OSI Approved :: BSD License',
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.8',
-            'Programming Language :: Python :: 3.9',
-            'Programming Language :: Python :: 3.10',
-            'Programming Language :: Python :: 3.11'
-        ]
+    name='sslearn',
+    version=version,
+    description='A Python package for semi-supervised learning with scikit-learn',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='José Luis Garrido-Labrador',
+    author_email='jlgarrido@ubu.es',
+    url='https://github.com/jlgarridol/sslearn',
+    license='new BSD',
+    download_url=url,
+    install_requires=parse_requirements('requirements.txt'),
+    packages=setuptools.find_packages(exclude=("tests", "experiments")),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11'
+    ]
 )
