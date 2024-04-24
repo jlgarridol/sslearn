1ca7f9aaf298e9308d1030c9aecb7b9b0ff00293
José Luis Garrido-Labrador <jlgarrido@ubu.es>
2024-04-24 12:59:36 +0200

    Add docs folder

--- docs/conf.py
+++ docs/conf.py
@@ -11,18 +11,8 @@ release = '1.0.4.1'
 # -- General configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
 ]
 
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
-intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
