5d5891ba6534278a78a57dc722ef0f201dacf04f
José Luis Garrido-Labrador <jlgarrido@ubu.es>
2024-04-24 18:47:17 +0200

    Document all repository using pdoc

--- sslearn/__init__.py
+++ sslearn/__init__.py
@@ -1,6 +1,10 @@
-# Open README.md and added to __doc__
-with open("../README.md", "r") as f:
-    __doc__ = f.read()
+# Open README.md and added to __doc__ for 
+import os
+if os.path.exists("../README.md"):
+    with open("../README.md", "r") as f:
+        __doc__ = f.read()
+else:
+    __doc__ = "Semi-Supervised Learning (SSL) is a Python package that provides tools to train and evaluate semi-supervised learning models."
 
 
 __version__='1.0.4.1'
